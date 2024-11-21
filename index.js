0f1673e00ac4b41238e91c3cb506a9290482684a
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-21 15:04:59 +0100

    11. Start by declaring a denyList variable. Assign it an array containing your helpRegex.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
+const denyList = [helpRegex];
 
 const isSpam = (msg) => helpRegex.test(msg);
 
